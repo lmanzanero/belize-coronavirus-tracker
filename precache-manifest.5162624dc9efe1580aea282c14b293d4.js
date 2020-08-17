822af3de10b1282472947fec83cd74025ac2cfcd
Luis Manzanero <manzanero.luis1995@gmail.com>
2020-08-17 12:47:29 +0800

    Updates

---
+++ precache-manifest.5162624dc9efe1580aea282c14b293d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d68dd5adeaf3a0a1d2d4a236dcb3a51",
+    "url": "/belize-coronavirus-tracker/index.html"
+  },
+  {
+    "revision": "5ad7322c8ace7c93c23c",
+    "url": "/belize-coronavirus-tracker/static/css/main.7608dceb.chunk.css"
+  },
+  {
+    "revision": "c75ebf74337f6c4727eb",
+    "url": "/belize-coronavirus-tracker/static/js/2.9116bc30.chunk.js"
+  },
+  {
+    "revision": "4db0b4b7fd2c7a0aaf4845dcd6ed7f11",
+    "url": "/belize-coronavirus-tracker/static/js/2.9116bc30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ad7322c8ace7c93c23c",
+    "url": "/belize-coronavirus-tracker/static/js/main.1bb236eb.chunk.js"
+  },
+  {
+    "revision": "bb6c701cbadc24896382",
+    "url": "/belize-coronavirus-tracker/static/js/runtime-main.ba573399.js"
+  },
+  {
+    "revision": "718e720a7400d83c42c6b250d3912b5b",
+    "url": "/belize-coronavirus-tracker/static/media/belize-flag.718e720a.png"
+  }
+]);
