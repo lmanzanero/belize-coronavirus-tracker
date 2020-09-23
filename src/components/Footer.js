84185cc02bd390e0729ed91e668351820b0b5f37
Luis Manzanero <manzanero.luis1995@gmail.com>
2020-09-23 23:30:46 +0800

    updated footer styles for contributors

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,15 +2,16 @@ import React from 'react';
 import { useQuery } from 'react-query'
 import { getGithubContributors } from '../repository/api';
 export const Footer = () => {
-  const { data, status, error, isLoading } = useQuery('getgithubcontributors', getGithubContributors)  
+  const { data, status, error, isLoading } = useQuery('getgithubcontributors', getGithubContributors) 
+  console.log(data); 
   return (
       <div className="footer"> 
         <p>This site is to show the latest on coronavirus cases based on official reports made from Belize. If you would like to contribute to the source code, feel free to do so <a href="https://github.com/lmanzanero/belize-coronavirus-tracker" target="_blank" rel="noopener noreferrer">here</a><span role="img" alt="globe"> 🌎🇧🇿</span></p>
         <div className="contributors">
-          <p>All Contributors</p>
+        <p><i>All Contributors</i></p>
           {
-            isLoading ? 'loading...' : data.data.map(contributor => 
-                <img key={contributor.id} src={contributor.avatar_url} />   
+            isLoading ? 'loading...' : data.data.map(contributor =>  
+                <a href={contributor.html_url} target="_blank"><img key={contributor.id} src={contributor.avatar_url} /></a> 
               )
           }
         </div>
