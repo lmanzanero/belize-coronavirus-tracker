37e53f17e54a9358ce04ae35616ca23d51d870c3
Luis Manzanero <manzanero.luis1995@gmail.com>
2020-10-02 01:10:49 +0800

    added api data to live timeline chart

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { useQuery } from 'react-query'
 import { getGithubContributors } from '../repository/api';
 export const Footer = () => {
-  const { data, isLoading } = useQuery('getgithubcontributors', getGithubContributors); 
+  // const { data, isLoading } = useQuery('getgithubcontributors', getGithubContributors); 
   return (
       <div className="footer"> 
         <p>This site is to show the latest on coronavirus cases based on official reports made from Belize. If you would like to contribute to the source code, feel free to do so <a href="https://github.com/lmanzanero/belize-coronavirus-tracker" target="_blank" rel="noopener noreferrer">here</a><span role="img" aria-label="globe" alt="globe"> 🌎🇧🇿</span></p>
         <div className="contributors">
-        <p><i>All Contributors</i> <span>({isLoading ? 'loading...' : data.data.length})</span></p>
+        {/* <p><i>All Contributors</i> <span>({isLoading ? 'loading...' : data.data.length})</span></p>
           {
             isLoading ? 'loading...' : data.data.map(contributor =>  
                 <a href={contributor.html_url} key={contributor.id} target="_blank" rel="noopener noreferrer"><img src={contributor.avatar_url} alt="users" /></a> 
               )
-          }
+          } */}
         </div>
       </div>
   );
--- src/components/charts/LiveTimelineChart.js
+++ src/components/charts/LiveTimelineChart.js
@@ -2,22 +2,22 @@ import React, { useEffect } from 'react';
 import { useQuery } from 'react-query';
 import Chart from 'chart.js'
 import { getCasesSinceDayOne } from '../../repository/api';
-import moment from 'moment';
+import moment from 'moment';   
 
 
 export default function LiveTimelineChart() {
-  const { isLoading } = useQuery('timelinecases', getCasesSinceDayOne);
+  const { data, isLoading } = useQuery('timelinecases', getCasesSinceDayOne);
   // const months = ['January','February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  // console.log(data);
-  let color = Chart.helpers.color;
-  useEffect(() => {
+	let color = Chart.helpers.color;
+	
+  useEffect(() => { 
     let cfg = {
 			data: {
 				datasets: [{
-					label: 'Cases by Day',
+					label: ' 🦠 Cases',
 					backgroundColor: color('red').alpha(0.5).rgbString(),
 					borderColor: 'rgba(54, 162, 235, 0.5)',
-					data: generateData(),
+					data: cleanUpData(),
 					type: 'line',
 					pointRadius: 0,
 					fill: false,
@@ -48,9 +48,10 @@ export default function LiveTimelineChart() {
 							sampleSize: 100
 						},
 						afterBuildTicks: function(scale, ticks) {
-							var majorUnit = scale._majorUnit;
-							var firstTick = ticks[0];
-							var i, ilen, val, tick, currMajor, lastMajor;
+							console.log(ticks[0].value);
+							let majorUnit = scale._majorUnit;
+							let firstTick = ticks[0];
+							let i, ilen, val, tick, currMajor, lastMajor;
 
 							val = moment(ticks[0].value);
 							if ((majorUnit === 'minute' && val.second() === 0)
@@ -80,7 +81,7 @@ export default function LiveTimelineChart() {
 						},
 						scaleLabel: {
 							display: true,
-							labelString: '# of cases'
+							labelString: '# of 🦠 cases'
 						}
 					}]
 				},
@@ -99,69 +100,29 @@ export default function LiveTimelineChart() {
 					}
 				}
 			}
-		};
-      let ctx = document.getElementById('liveCasesByTimelineCart');
-      let liveCasesByTimelineCart = new Chart(ctx, cfg);
-  function generateData() {
-    let unit = document.getElementById('unit').value;
-   
-     function unitLessThanDay() {
-       return unit === 'second' || unit === 'minute' || unit === 'hour';
-     }
-   
-     function beforeNineThirty(date) {
-       return date.hour() < 9 || (date.hour() === 9 && date.minute() < 30);
-     }
-   
-     // Returns true if outside 9:30am-4pm on a weekday
-     function outsideMarketHours(date) {
-       if (date.isoWeekday() > 5) {
-         return true;
-       }
-       if (unitLessThanDay() && (beforeNineThirty(date) || date.hour() > 16)) {
-         return true;
-       }
-       return false;
-     }
-   
-     function randomNumber(min, max) {
-       return Math.random() * (max - min) + min;
-     }
-   
-     function randomBar(date, lastClose) {
-      let open = randomNumber(lastClose * 0.95, lastClose * 1.05).toFixed(2);
-      let close = randomNumber(open * 0.95, open * 1.05).toFixed(2);
-       return {
-         t: date.valueOf(),
-         y: close
-       };
-     }
-   
-    let date = moment('Jan 01 1990', 'MMM DD YYYY');
-    let now = moment();
-    let data = [];
-    let lessThanDay = unitLessThanDay();
-     for (; data.length < 600 && date.isBefore(now); date = date.clone().add(1, unit).startOf(unit)) {
-       if (outsideMarketHours(date)) {
-         if (!lessThanDay || !beforeNineThirty(date)) {
-           date = date.clone().add(date.isoWeekday() >= 5 ? 8 - date.isoWeekday() : 1, 'day');
-         }
-         if (lessThanDay) {
-           date = date.hour(9).minute(30).second(0);
-         }
-       }
-       data.push(randomBar(date, data.length > 0 ? data[data.length - 1].y : 30));
-     }
-   
-     return data;
-   }
+		}
 
-  // console.log(generateData())
-}, []) 
+	if(!isLoading){ 
+		let ctx = document.getElementById('liveCasesByTimelineCart');
+		let liveCasesByTimelineCart = new Chart(ctx, cfg);
+	}
+      
+	 function cleanUpData() {
+		if(data){
+			const dataArr = data.data.map(cases => { 
+				const chartData = {
+						t: new Date(cases.Date).getTime(),
+						y: `${cases.Cases}`
+				}
+				return chartData
+			}) 
+			return dataArr;
+		} 
+	}  
 
- 
+}, [isLoading]);
 
-  return (
+	return (
     <div className={`chart ${isLoading ? 'loading' : ''}`}>  
       <div className="chart-inner">  
         <canvas id="liveCasesByTimelineCart" style={{margin: '0', padding:0}}></canvas> 
